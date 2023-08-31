1e5b78aa0c84cdc3da08536deb9d10c3a3b97e8e
irene0196 <142917090+irene0196@users.noreply.github.com>
2023-08-31 17:33:26 +0100

    updated the api for weather information

---
+++ src/app.js
@@ -0,0 +1,46 @@
+let apiKey = "4f2de4faea20e4o3bbf33tf295b054b5";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=London&key=4f2de4faea20e4o3bbf33tf295b054b5&units=metric";
+console.log(apiUrl);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayList = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayList];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+axios.get(apiUrl).then(displayTemperature);
